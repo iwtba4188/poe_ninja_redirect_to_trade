709f6d46b794fc406d640d3ec71c54c79a4f647c
iwtba4188 <X69599596@gmail.com>
2024-05-30 01:37:04 +0800

    docs: add comments of code

--- background.js
+++ background.js
@@ -11,16 +11,22 @@ fetch(STATS_DATA_PATH).then((response) => response.json()).then((json) => stats_
 var query_data;
 fetch(QUERY_PATH).then((response) => response.json()).then((json) => query_data = json);
 
+/**
+ * 使用 fecth 方法取得該網頁的資料
+ * @method fetch_url
+ * @param {string} target_url 目標網頁，在此應為 poe.ninja 網頁網址
+ * @returns {string} @param target_url 轉換為 JSON 的結果
+ */
 async function fetch_url(target_url) {
+
     var res;
 
     await fetch(target_url).then(
         function (response) {
-            if (response.status === 200) {
+            if (response.status === 200)
                 return response.json();
-            } else {
+            else
                 throw new Error("Request failed: " + response.status);
-            }
         }
     ).then(function (data) {
         // console.log(_data);
@@ -33,9 +39,15 @@ async function fetch_url(target_url) {
     return res;
 };
 
-// 利用取得的url資訊取得所需response body。
+/**
+ * 利用取得的角色資訊，內含本專案所需之裝備資料
+ * @method fetch_character_data
+ * @param {any} details 詳見 google extension webRequest api
+ * @return {None}
+ */
 async function fetch_character_data(details) {
-    // 去除監聽器避免抓取此函式發出的request。
+
+    // 去除監聽器避免抓取此函式發出的 request 導致無限循環
     chrome.webRequest.onCompleted.removeListener(fetch_character_data);
 
     var api_url = details.url;
@@ -49,7 +61,16 @@ async function fetch_character_data(details) {
     });
 }
 
+/**
+ * 要 inject 進目前 tab 的 script，功能：加入按鈕，轉換物品 mod 到 stats id
+ * @method inject_script
+ * @param {Object} stats_data 所有詞墜表，格式詳見 POE 官網
+ * @param {Object} query_data poe trade 的 query 格式，詳見 POE 官網及 query_example.json 示範
+ * @param {Object} equipment_data 抓取到的角色裝備資料，內容來源為 poe.ninja，但格式是 POE 官方定義的
+ * @return {None}
+ */
 async function inject_script(stats_data, query_data, equipment_data) {
+
     const POE_TRADE_URL = "https://www.pathofexile.com/trade/search";
     const BALANCE_ICON = `<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -63,32 +84,41 @@ async function inject_script(stats_data, query_data, equipment_data) {
     // console.log(query_data);
     // console.log(equipment_data);
 
-    function cal_difference(int1, int2) {
-        if (int1 > int2) return int1 - int2;
-        else if (int2 > int1) return int2 - int1;
-    };
+    /**
+     * 從 STATS_DATA_PATH 尋找 mod_string 對應的 stats id
+     * @method find_mod_id
+     * @param {string} mod_string 要查詢的詞墜（預先處理過），格式實際上是 RegExp，用來直接使用正則表達式查詢 stats id
+     * @param {number} mod_type_index 詞墜種類對應 stats 資料的 index。TODO: 把 index 看能不能 ENUM 起來
+     * @return {string} 查詢到的 stats id，如 enchant.stat_2954116742。如果沒有查詢到的話，則為空字串。
+     */
+    function find_mod_id(mod_string, mod_type_index) {
 
-    function find_mod_id(stat_string, stat_index) {
-        var entries = stats_data["result"][stat_index]["entries"];
-        stat_string = stat_string.replace(/[\+]+/, "\\+");
-        stat_string = stat_string.replace(/^[-]+/, "");
-        stat_string = stat_string.replace(/[\d.]+/g, ".+?");
+        var stats_entries = stats_data["result"][mod_type_index]["entries"];
+        mod_string = mod_string.replace(/[\+]+/, "\\+");
+        mod_string = mod_string.replace(/^[-]+/, "");
+        mod_string = mod_string.replace(/[\d.]+/g, ".+?");
 
         var res_id = "";
         var res_text = "";
-        Object.values(entries).some(function (key) {
-            if (RegExp(stat_string).test(key["text"])) {
-                if (Math.abs(key.text.length - stat_string.length) < Math.abs(res_text.length - stat_string.length)) {
+        Object.values(stats_entries).some(function (key) {
+            // stats_entries 中每一項的 text 如果符合想查找的詞墜，也就是 @mod_string 是該項 text 的子字串
+            if (RegExp(mod_string).test(key.text)) {
+                // 如果找到長度更接近 @mod_string 的話，更新結果
+                if (Math.abs(key.text.length - mod_string.length) < Math.abs(res_text.length - mod_string.length)) {
                     [res_id, res_text] = [key.id, key.text];
                 }
             }
         });
 
-        if (res_id.length === 0) console.log(stat_index + ": " + stat_string);
+        // DEBUG 資訊，當未查詢到該詞墜的話，印出該詞墜種類及詞墜本身
+        if (res_id.length === 0) console.log(mod_type_index + ": " + mod_string);
         // console.log(res);
         return res_id;
     };
 
+    /**
+     * 修正 enchant 類別的詞墜
+     */
     function fix_enchant(mod, item_type) {
         var filter = { "id": -1 };
         // fix "Allocates #"
@@ -132,80 +162,48 @@ async function inject_script(stats_data, query_data, equipment_data) {
         return [mod, filter];
     };
     function fix_explicit(mod, item_type) {
-        var filter = { "id": -1 };
+        const filter = { id: -1 };
+        const localMods = [
+            { regex: /(Evasion and Energy Shield)$/, replace: "Evasion and Energy Shield (Local)" },
+            { regex: /(increased Evasion Rating$)/, replace: "increased Evasion Rating (Local)" },
+            { regex: /(increased Armour$)/, replace: "increased Armour (Local)" },
+        ];
+        const globalMods = [
+            { regex: /(^[^+]\d+% Chance to Block Attack Damage)/, replace: "#% Chance to Block Attack Damage" },
+            { regex: /(^[^+]\d+% Chance to Block Spell Damage)/, replace: "#% Chance to Block Spell Damage" },
+            { regex: /(reduced Global Physical Damage)/, replace: "increased Global Physical Damage" },
+            { regex: /(Non-Channelling Skills have .+ to Total Mana Cost)/, replace: "Non-Channelling Skills have +# to Total Mana Cost" },
+        ];
+        const flaskMods = [
+            { regex: /(Charges|Charge)/, replace: "(Charges|Charge)" },
+            { regex: /(reduced \(Charges\|Charge\) per use)/, replace: "increased (Charges|Charge) per use" },
+            { regex: /(reduced Amount Recovered)/, replace: "increased Amount Recovered" },
+            { regex: /(reduced Effect of Shock on you during Effect)/, replace: "increased Effect of Shock on you during Effect" },
+            { regex: /(reduced Duration)/, replace: "increased Duration" },
+            { regex: /(Consumes Frenzy Charges on use)/, replace: "Consumes 1 Frenzy Charge on use" },
+            { regex: /(less Duration)/, replace: "more Duration" },
+            { regex: /(reduced Charges per use)/, replace: "increased Charges per use" },
+        ];
 
-        if (item_type === "items") {
-            // fix "#% Chance to Block Attack Damage"
-            mod = mod.replace(/(^[^+]\d+% Chance to Block Attack Damage)/, "#% Chance to Block Attack Damage");
-            // fix "#% Chance to Block Spell Damage"
-            mod = mod.replace(/(^[^+]\d+% Chance to Block Spell Damage)/, "#% Chance to Block Spell Damage");
-            // fix "Items and Gems have #% reduced Attribute Requirements"
-            mod = mod.replace(/(reduced Attribute Requirements)/, "increased Attribute Requirements");
-            // fix "#% reduced Global Physical Damage"
-            mod = mod.replace(/(reduced Global Physical Damage)/, "increased Global Physical Damage");
-            // fix "Shocks you inflict spread to other Enemies within 1.5 metres"
-            mod = mod.replace(/(within 1.5 metres)/, "within 1.5 metre");
-            // fix "Leftmost # Magic Utility Flasks constantly apply their Flask Effects to you"
-            // mod = mod.replace(/(Magic Utility Flasks constantly apply their Flask Effects to you)/, "Magic Utility Flask constantly applies its Flask Effect to you");
-            mod = mod.replace(/(Flasks|Flask)/, "\(Flasks|Flask\)");
-            mod = mod.replace(/(applies|apply)/, "\(applies|apply\)");
-            mod = mod.replace(/(Effects|Effect)/, "\(Effects|Effect\)");
-            mod = mod.replace(/(its|their)/, "\(its|their\)");
-            // fix "#% increased Evasion and Energy Shield"
-            mod = mod.replace(/(Evasion and Energy Shield)/, "Evasion and Energy Shield \\(Local\\)");
-            // fix "#% Chance to Block Attack Damage while wielding a Staff"
-            mod = mod.replace(/(% Chance to Block Attack Damage while wielding a Staff)/, "% Chance to Block Attack Damage while wielding a Staff \\(Staves\\)");
-            // fix "#% increased Evasion Rating"
-            mod = mod.replace(/(increased Evasion Rating$)/, "increased Evasion Rating \\(Local\\)");
-            // fix "#% increased Armour"
-            mod = mod.replace(/(increased Armour$)/, "increased Armour \\(Local\\)");
-            // fix "Non-Channelling Skills have -# to Total Mana Cost"
-            mod = mod.replace(/(Non-Channelling Skills have .+ to Total Mana Cost)/, "Non-Channelling Skills have \+# to Total Mana Cost");
-        } else if (item_type === "flasks") {
-            mod = mod.replace(/(Charges|Charge)/, "\(Charges|Charge\)");
-            // fix "#% reduced Charges per use"
-            mod = mod.replace(/(reduced \(Charges\|Charge\) per use)/, "increased \(Charges|Charge\) per use");
-            // fix "#% reduced Amount Recovered"
-            mod = mod.replace(/(reduced Amount Recovered)/, "increased Amount Recovered");
-            // fix "#% reduced Effect of Shock on you during Effect"
-            mod = mod.replace(/(reduced Effect of Shock on you during Effect)/, "increased Effect of Shock on you during Effect");
-            // fix "+#% reduced Duration"
-            mod = mod.replace(/(reduced Duration)/, "increased Duration");
-            // fix "Consumes Frenzy Charges on use"
-            mod = mod.replace(/(Consumes Frenzy Charges on use)/, "Consumes 1 Frenzy Charge on use");
-            // fix "less Duration"
-            mod = mod.replace(/(less Duration)/, "more Duration");
-            // fix "#% reduced Charges per use"
-            mod = mod.replace(/(reduced Charges per use)/, "increased Charges per use");
-        } else if (item_type === "jewels") {
-            // fix "Passives in Radius of # can be Allocated\nwithout being connected to your tree"
-            if (/Passives in Radius of .+ can be Allocated\nwithout being connected to your tree/.test(mod)) {
-                var passives_list = stats_data["result"][1]["entries"][1787]["option"]["options"];
+        const mods = item_type === "items" ? [...localMods, ...globalMods] : item_type === "flasks" ? flaskMods : [];
 
-                var passive_name = mod.match(/Passives in Radius of (.+) can be Allocated/)[1];
-                console.log(passive_name);
-                var passive_id = -1;
-                Object.values(passives_list).some(function (passives) {
-                    if (passives.text === passive_name) passive_id = passives.id;
-                });
-
-                filter["value"] = { "option": passive_id };
-                mod = "Passives in Radius of # can be Allocated\nwithout being connected to your tree";
-            }
+        for (const { regex, replace } of mods) {
+            mod = mod.replace(regex, replace);
         }
 
-        // fix "Grants Summon Harbinger"
+        if (/Passives in Radius of .+ can be Allocated\nwithout being connected to your tree/.test(mod)) {
+            const passivesList = stats_data.result[1].entries[1787].option.options;
+            const passiveName = mod.match(/Passives in Radius of (.+) can be Allocated/)[1];
+            const passiveId = Object.values(passivesList).find((passives) => passives.text === passiveName)?.id ?? -1;
+            filter.value = { option: passiveId };
+            mod = "Passives in Radius of # can be Allocated\nwithout being connected to your tree";
+        }
+
         if (/Grants Summon.+?Harbinger/.test(mod)) {
-            var passives_list = stats_data["result"][1]["entries"][1125]["option"]["options"];
-
-            var passive_name = mod.match(/Grants Summon (.+) Skill/)[1];
-            console.log(passive_name);
-            var passive_id = -1;
-            Object.values(passives_list).some(function (passives) {
-                if (passives.text === passive_name) passive_id = passives.id;
-            });
-
-            filter["value"] = { "option": passive_id };
+            const passivesList = stats_data.result[1].entries[1125].option.options;
+            const passiveName = mod.match(/Grants Summon (.+) Skill/)[1];
+            const passiveId = Object.values(passivesList).find((passives) => passives.text === passiveName)?.id ?? -1;
+            filter.value = { option: passiveId };
             mod = "Grants Summon Harbinger";
         }
 
@@ -224,80 +222,30 @@ async function inject_script(stats_data, query_data, equipment_data) {
 
     // type: items, flasks, jewels
     async function generate_target_url(item_index, item_type) {
-        var this_query = JSON.parse(JSON.stringify(query_data));
-        var item_data = equipment_data[item_type][item_index]["itemData"];
+        const this_query = JSON.parse(JSON.stringify(query_data));
+        const equipment = equipment_data[item_type][item_index];
+        const mods = [
+            { key: "enchantMods", index: 4, fix: fix_enchant },
+            { key: "implicitMods", index: 2, fix: fix_implicit },
+            { key: "fracturedMods", index: 3, fix: fix_fractured },
+            { key: "explicitMods", index: 1, fix: fix_explicit },
+            { key: "craftedMods", index: 6, fix: fix_crafted },
+        ];
 
-        // enchant mods not empty
-        // enchant stat index = 4
-        if (item_data["enchantMods"].length !== 0) {
-            for (const mod of item_data["enchantMods"]) {
-                var [fixed_mod, filter] = fix_enchant(mod, item_type);
-                var mod_id = find_mod_id(fixed_mod, 4);
+        for (const { key, index, fix } of mods) {
+            const item_mods = equipment.itemData[key];
+            if (item_mods.length === 0) continue;
+
+            for (const mod of item_mods) {
+                const [fixed_mod, filter] = fix(mod, item_type);
+                const mod_id = find_mod_id(fixed_mod, index);
 
                 if (mod_id.length !== 0) {
-                    filter["id"] = mod_id;
-                    this_query["query"]["stats"][0]["filters"].push(filter);
+                    filter.id = mod_id;
+                    this_query.query.stats[0].filters.push(filter);
                 }
             }
         }
-
-        // implicit mods not empty
-        // implicit stat index = 2
-        if (item_data["implicitMods"].length !== 0) {
-            for (const mod of item_data["implicitMods"]) {
-                var [fixed_mod, filter] = fix_implicit(mod, item_type);
-                var mod_id = find_mod_id(fixed_mod, 2);
-
-                if (mod_id.length !== 0) {
-                    filter["id"] = mod_id;
-                    this_query["query"]["stats"][0]["filters"].push(filter);
-                }
-            }
-        }
-
-        // fractured mods not empty
-        // fractured stat index = 3
-        if (item_data["fracturedMods"].length !== 0) {
-            for (const mod of item_data["fracturedMods"]) {
-                var [fixed_mod, filter] = fix_fractured(mod, item_type);
-                var mod_id = find_mod_id(fixed_mod, 3);
-
-                if (mod_id.length !== 0) {
-                    filter["id"] = mod_id;
-                    this_query["query"]["stats"][0]["filters"].push(filter);
-                }
-            }
-        }
-
-        // explicit mods not empty
-        // explicit stat index = 1
-        if (item_data["explicitMods"].length !== 0) {
-            for (const mod of item_data["explicitMods"]) {
-                var [fixed_mod, filter] = fix_explicit(mod, item_type);
-                var mod_id = find_mod_id(fixed_mod, 1);
-
-                if (mod_id.length !== 0) {
-                    filter["id"] = mod_id;
-                    this_query["query"]["stats"][0]["filters"].push(filter);
-                }
-            }
-        }
-
-        // crafted mods not empty
-        // crafted stat index = 6
-        if (item_data["craftedMods"].length !== 0) {
-            for (const mod of item_data["craftedMods"]) {
-                var [fixed_mod, filter] = fix_crafted(mod, item_type);
-                var mod_id = find_mod_id(fixed_mod, 6);
-
-                if (mod_id.length !== 0) {
-                    filter["id"] = mod_id;
-                    this_query["query"]["stats"][0]["filters"].push(filter);
-                }
-            }
-        }
-
-        // console.log(JSON.stringify(this_query));
 
         return JSON.stringify(this_query);
     };
@@ -353,7 +301,7 @@ async function inject_script(stats_data, query_data, equipment_data) {
     add_button_to_page();
 };
 
-// 當頁面建立或重新整理時，擷取指定封包資訊以利用url資訊取得所需response body。
+// 當頁面建立或重新整理時，擷取送出的封包以取得能拿到角色資料的api網址
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === "complete") {
         console.log(tabId + " is updated!");
